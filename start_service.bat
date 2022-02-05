756a365cf14cfd271d87da8aaecb92e511421876
ROCK ARMY <95131805+Innocentarmy1@users.noreply.github.com>
2022-02-05 06:40:34 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop RocksAlexaRobot
-net start RocksAlexaRobot
+net stop RFCMUSIC
+net start RFCMUSIC
